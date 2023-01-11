442492191ebcb7c2aa7fb47b3229fb1aaecf53d2
FalaisePocket <106278663+FalaisePocket@users.noreply.github.com>
2023-01-10 21:01:38 -0500

    Ejemplos añadidos,nashe
    Golden unworldly silence

--- Mini-Py.rkt
+++ Mini-Py.rkt
@@ -789,9 +789,16 @@
 ;Enteros
 (scan&parse "7")
 
+
 ;Flotantes
 (scan&parse "4.4")
 
+;Numerosen base 8
+(scan&parse "x8(4 5 6)")
+;numerosen base 16
+(scan&parse "x16(15 4 13)")
+;numeros en base 32
+(scan&parse "x32( 31 24 2 14)")
 ;Cadena
 (scan&parse "\"hola\"")
 
@@ -811,6 +818,11 @@
 
 ;Actualización de variable
 (scan&parse "var @l = 7 in begin set @l = 10; @l end")
+
+;rec
+
+
+
 
 ;procedimientos
 (scan&parse "function(@a, @b, @c) {((@a + @b)*@c)}")
@@ -834,9 +846,61 @@
 (scan&parse "cola (crear-lista(5,4,3))") ;cola
 (scan&parse "append ([5,7,9], [9,4,10])") ;append
 (scan&parse "ref-list([0,7,15],2)") ;ref
+;tupla
+(scan&parse "crear-tupla(2,3,4,5,6)")
+(scan&parse "tupla?(crear-tupla(2,3,4,5,6))")
+(scan&parse "ref-tupla(crear-tupla(2,3,4,5,6),3)")
+
 
 ;registros
 (scan&parse "crear-registro(@a = 5, @b = 6)") ;crear registro
 (scan&parse "registro?(crear-registro(@g=8,@h=6))") ;registro?
 (scan&parse "ref-registro({@f = 7, @g = 5}, @g)") ;ref registro
 (scan&parse "set-registro({@f = 7, @g = 5},@f, 1)") ;set registro
+
+
+;;;;estructuras de control
+(scan&parse "begin ; end")
+(scan&parse "if <(3,5) : (6+7) else : (7-6) end")
+(scan&parse "while : done")
+(scan&parse "for(@hw = 5 to 10 ){ print(5) }")
+;operaciones aritmeticas
+(scan&parse "(1 + 1)")
+(scan&parse "(1 ~ 2)")
+(scan&parse "(1 * 2)")
+(scan&parse "(1 / 2)")
+(scan&parse "(4 % 2")
+(scan&parse "(x8(3) +(x8) x8(4))")
+(scan&parse "(x16(15 5) +(x16) x16(10 3))")
+(scan&parse "(x32(23) +(x32) x32(4))")
+(scan&parse "(x8(3) *(x8) x8(4))")
+(scan&parse "(x16(15) *(x16) x16(1))")
+(scan&parse "(x32(14 3) *(x32) x32(24))")
+(scan&parse "(x8(7) ~(x8) x8(4))")
+(scan&parse "(x16(15) ~(x16) x16(15))")
+(scan&parse "(x32(3) ~(x32) x32(31))")
+;primitivas unarias
+(scan&parse "sub1(4)")
+(scan&parse "add1(2)")
+(scan&parse "add1(x8)(x8(2))")
+(scan&parse "add1(x16)(x16(14))")
+(scan&parse "add1(x16)(x16(26))")
+;primitivas sobre cadenas
+(scan&parse "sub1(4)")
+;invocacion de procedimientos
+(scan&parse "function (@a,@b){(@a+@b)}")
+(scan&parse "evaluar ( (1+2) )")
+;weas de verdadero o falso
+(scan&parse "> (3,5)")
+(scan&parse "< (4,2)")
+(scan&parse ">= (6,8)")
+(scan&parse ">= (7,9)")
+(scan&parse "== (7,7)")
+(scan&parse "!= (2,0)")
+(scan&parse "or(true,false)")
+(scan&parse "and(true,true)")
+(scan&parse "not(true)")
+;funcion print
+(scan&parse "print(true)")
+;
+
